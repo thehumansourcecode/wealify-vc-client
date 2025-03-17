788b20588ced235f59ac7c4bb16ac0e34e2d7ebe
Brennan Ngo(Anh Ngo) <88129967+brennan992k@users.noreply.github.com>
2025-03-17 11:25:00 +0700

    auth image

---
+++ layouts/auth.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import '~/assets/css/style.scss'
+</script>
+
+<template>
+  <div class="">
+    <slot />
+  </div>
+</template>
+
+<style lang="scss">
+::-webkit-scrollbar {
+  width: 4px;
+}
+::-webkit-scrollbar-thumb {
+  background: #888888;
+}
+::-webkit-scrollbar-track {
+  background: #f0f0f0;
+}
+</style>
---
+++ pages/signin.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'auth',
+})
+</script>
+<template>
+  <div class="flex flex-row justify-between w-full">
+    <div class="col-3 col-left"></div>
+    <div class="col-2 col-right"></div>
+  </div>
+</template>
