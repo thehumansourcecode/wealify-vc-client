41bb448b639c23cb7f4451bc892294a84dc25d2d
Brennan Ngo(Anh Ngo) <anh.nh182113@gmail.com>
2025-03-18 13:14:21 +0700

    reset page

---
+++ components/auth/reset/countdown.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+// Define props with TypeScript
+const props = defineProps<{
+  target: string | Date | number
+  onComplete?: () => void
+}>()
+
+// Use the countdown composable
+const { time } = useCountdown(props.target, props.onComplete)
+</script>
+<template>
+  <span> <slot v-if="time > 0" name="countdown" :time="time"></slot> <slot v-else name="complete"></slot></span>
+</template>
---
+++ composables/auth/useReset.ts
@@ -0,0 +1,91 @@
+interface ResetFields {
+  otp: string
+  password: string
+  confirmPassword: string
+}
+
+interface ResetErrors {
+  otp: string | undefined
+  password: string | undefined
+  confirmPassword: string | undefined
+}
+
+export const useReset = () => {
+  const { profile } = useProfileStore()
+  const router = useRouter()
+  const step = ref<1 | 2 | 3>(1)
+  const toast = useToast()
+
+  const fields = ref<ResetFields>({
+    otp: '',
+    password: '',
+    confirmPassword: '',
+  })
+
+  const errors = ref<ResetErrors>({
+    otp: undefined,
+    password: undefined,
+    confirmPassword: undefined,
+  })
+
+  const back = () => {
+    switch (step.value) {
+      case 1:
+        router.push('/auth/sign-in')
+        break
+      case 2:
+      case 3:
+        step.value -= 1
+        break
+      default:
+        break
+    }
+  }
+
+  const next = () => {
+    switch (step.value) {
+      case 1:
+        step.value += 1
+        break
+      case 2:
+        toast.add({
+          title: 'Password changed successfully',
+          timeout: 5000,
+        })
+        router.push('/auth/sign-in')
+        break
+      default:
+        break
+    }
+  }
+
+  const onCompletedPin = (pin: string) => {
+    fields.value.otp = pin
+    next()
+  }
+
+  const isValidate = computed(
+    () =>
+      fields.value.password &&
+      fields.value.confirmPassword &&
+      fields.value.password == fields.value.confirmPassword &&
+      !errors.value.password &&
+      !errors.value.confirmPassword,
+  )
+
+  watch(
+    () => fields.value.password,
+    () => {
+      errors.value.password = validatePassword(fields.value.password)
+    },
+  )
+
+  watch(
+    () => fields.value.confirmPassword,
+    () => {
+      errors.value.confirmPassword = validateConfirmPassword(fields.value.confirmPassword, fields.value.password)
+    },
+  )
+
+  return { step, back, next, isValidate, profile, onCompletedPin, fields, errors }
+}
