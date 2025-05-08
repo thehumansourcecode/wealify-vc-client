d833fb614901614d668e538893a4330f5acfbe31
khoand <neu.khoanguyen@gmail.com>
2025-05-08 09:37:00 +0700

    rollback

--- stores/common.ts
+++ stores/common.ts
@@ -9,47 +9,96 @@ import {
   type IDropdownCardData,
   type IFeeData,
 } from '~/types/common'
-import { defineStore } from 'pinia'
 
-export const useCommonStore = defineStore('common', {
-  state: () => ({
-    activeTab: PanelTab.DASHBOARD,
-    activeChildTab: '',
-    isHeaderBackLayout: false,
-    isMobileMenuOpen: false,
-    categoryList: [] as CardCategory[],
-    feeList: {} as Fees
-  }),
-  actions: {
-    setActiveTab(tab: PanelTab) {
-      this.activeTab = tab
-    },
-    setActiveChildTab(tab: string) {
-      this.activeChildTab = tab
-    },
-    setHeaderBackLayout(isBack: boolean) {
-      this.isHeaderBackLayout = isBack
-    },
-    toggleMobileMenu(isOpen?: boolean) {
-      if (typeof isOpen === 'boolean') {
-        this.isMobileMenuOpen = isOpen
-      } else {
-        this.isMobileMenuOpen = !this.isMobileMenuOpen
-      }
-    },
-    async getDropdownCategoryList() {
-      const response = await commonService.getDropdownCategoryList()
-      if (response.success) {
-        this.categoryList = response.data.items
-      }
-      return response
-    },
-    async getFee(type?: FeeType) {
-      const response = await commonService.getFee(type)
-      if (response.success) {
-        this.feeList = response.data
-      }
-      return response
+export const useCommonStore = defineStore('common', () => {
+  const config = useRuntimeConfig()
+  const { t } = useI18n()
+
+  const loading = ref(false)
+
+  function setLoading(state: boolean) {
+    loading.value = state
+  }
+
+  // Move to local storage
+  const currency = ref<CommonCurrency>(CommonCurrency.USD)
+
+  function setCurrency(c: CommonCurrency) {
+    currency.value = c
+  }
+
+  const activeTab = ref<PanelTab>(PanelTab.DASHBOARD)
+
+  function setActiveTab(tab: PanelTab) {
+    activeTab.value = tab
+  }
+
+  const activeChildTab = ref<PanelChildTab>()
+
+  function setActiveChildTab(tab: PanelChildTab | undefined) {
+    activeChildTab.value = tab
+  }
+
+  const isHeaderBackLayout = ref(false)
+
+  function setHeaderBackLayout(state: boolean) {
+    isHeaderBackLayout.value = state
+  }
+
+  const categoryList = ref<CardCategory[]>([])
+
+  async function getDropdownCategoryList() {
+    const response = await commonService.getDropdownCategoryList()
+    if (response.success) {
+      categoryList.value = response.data as CardCategory[]
     }
-  },
+    return response
+  }
+
+  const dropdownCardList = ref<IDropdownCardData[]>()
+
+  async function getDropdownCardList() {
+    const response = await commonService.getDropdownCardList({ card_status: [CardStatus.ACTIVE], page: 1, limit: 100 })
+    if (response.success) {
+      dropdownCardList.value = response.data as IDropdownCardData[]
+    }
+    return response
+  }
+
+  const isOpenProcessingModal = ref(false)
+  function toggleProcessingModal(state: boolean) {
+    isOpenProcessingModal.value = state
+  }
+
+  const feeList = ref<Fees>()
+
+  async function getFee() {
+    const response = await commonService.getFee()
+    if (response.success) {
+      feeList.value = response.data
+    }
+    return response
+  }
+
+  return {
+    config,
+    loading,
+    setLoading,
+    currency,
+    setCurrency,
+    activeTab,
+    setActiveTab,
+    isHeaderBackLayout,
+    setHeaderBackLayout,
+    activeChildTab,
+    setActiveChildTab,
+    categoryList,
+    getDropdownCategoryList,
+    dropdownCardList,
+    getDropdownCardList,
+    isOpenProcessingModal,
+    toggleProcessingModal,
+    feeList,
+    getFee,
+  }
 })
